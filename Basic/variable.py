d8f9da75d1c9bd73922e8d8c3713482da7b74c38
johnnynode <johnnynode@gmail.com>
2017-11-04 18:17:22 +0800

    'variable'

---
+++ Basic/variable.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+counter = 100  # 赋值整型变量
+miles = 1000.0  # 浮点型
+name = "John"  # 字符串
+
+print counter
+print miles
+print name
+
+'''
+$ python Basic/variable.py
+100
+1000.0
+John
+
+'''
