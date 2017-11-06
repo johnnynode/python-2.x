6f30c026aca883046395b7e2bd0cfeb7951ccffa
johnnynode <johnnynode@gmail.com>
2017-11-07 06:12:22 +0800

    while

---
+++ Basic/loop-while-1.py
@@ -0,0 +1,23 @@
+# coding=utf-8
+
+count = 0
+while (count < 9):
+    print 'The count is:', count
+    count = count + 1
+
+print "Good bye!"
+
+"""
+$ python Basic/loop-while-1.py
+The count is: 0
+The count is: 1
+The count is: 2
+The count is: 3
+The count is: 4
+The count is: 5
+The count is: 6
+The count is: 7
+The count is: 8
+Good bye!
+
+"""
