e1239d23c5a6dbe9e8368264f4e64c7c2a7d0636
johnnynode <johnnynode@gmail.com>
2017-11-07 06:06:56 +0800

    if

---
+++ Basic/if-3.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+num = 9
+if num >=0 and num <= 10:
+    print 'hello'
+
+num = 10
+if num < 0 or num > 10:
+    print 'hello'
+else:
+    print 'undefine'
+
+num = 8
+if (num >=0 and num <=5) or (num >= 10 and num <= 15):
+    print 'hello'
+else:
+    print 'undefine'
+
+"""
+$ python Basic/if-3.py
+hello
+undefine
+undefine
+
+"""
