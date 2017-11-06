949cd10f59c235ef48c16195ec96aab36da9b7d0
johnnynode <johnnynode@gmail.com>
2017-11-06 08:26:41 +0800

    operators

---
+++ Basic/operators-6.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5]
+
+if (a in list):
+    print "1 - 变量 a 在给定的列表 list 中"
+else:
+    print "1 - 变量 a 不在给定的列表 list 中"
+
+if (b not in list):
+    print "2 - 变量 b 不在给定的列表 list 中"
+else:
+    print "2 - 变量 b 在给定的列表 list 中"
+
+# 修改变量 a 的值
+a = 2
+if (a in list):
+    print "3 - 变量 a 在给定的列表 list 中"
+else:
+    print "3 - 变量 a 不在给定的列表 list 中"
+
+"""
+$ Python Basic/operators-6.py
+1 - 变量 a 不在给定的列表 list 中
+2 - 变量 b 不在给定的列表 list 中
+3 - 变量 a 在给定的列表 list 中
+
+"""
