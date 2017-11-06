6510911c9e5fa29cc9011d67f3e7932d0791e203
johnnynode <johnnynode@gmail.com>
2017-11-07 06:03:57 +0800

    if

---
+++ Basic/if-2.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+num = 5
+if num == 3:
+    print 'boss'
+elif num == 2:
+    print 'user'
+elif num == 1:
+    print 'worker'
+elif num < 0:
+    print 'error'
+else:
+    print 'roadman'
+
+"""
+$ python Basic/if-2.py
+roadman
+
+"""
