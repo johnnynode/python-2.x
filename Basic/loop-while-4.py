39eed0311bbc0232f47417cdd9cf917fe34592bc
johnnynode <johnnynode@gmail.com>
2017-11-07 06:27:11 +0800

    while

---
+++ Basic/loop-while-4.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+
+var = 1
+while var == 1:
+    var = 0
+    num = raw_input("Enter a num :")
+    print "You entered: ", num
+
+print "Good bye!"
+
+"""
+$ python Basic/loop-while-4.py
+hello
+Enter a num :You entered:  hello
+Good bye!
+
+"""
