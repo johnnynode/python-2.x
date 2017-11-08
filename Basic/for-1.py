215b60642514d8bc79bd4d20721b67fd563a8bd3
johnnynode <johnnynode@gmail.com>
2017-11-08 10:46:12 +0800

    for

---
+++ Basic/for-1.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+for letter in 'Python':
+    print '当前字母 ：', letter
+
+fruits = ['banana', 'apple', 'mango']
+for fruit in fruits:
+    print '当前水果 ：', fruit
+
+print "Good bye!"
+
+"""
+$ python Basic/for-1.py
+当前字母 ： P
+当前字母 ： y
+当前字母 ： t
+当前字母 ： h
+当前字母 ： o
+当前字母 ： n
+当前水果 ： banana
+当前水果 ： apple
+当前水果 ： mango
+Good bye!
+
+"""
