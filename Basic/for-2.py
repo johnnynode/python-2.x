f3991289b42bc148c81a98c641a91c5aa8e4f1d9
johnnynode <johnnynode@gmail.com>
2017-11-08 10:48:09 +0800

    for

---
+++ Basic/for-2.py
@@ -0,0 +1,16 @@
+# coding=utf-8
+
+fruits = ['banana', 'apple', 'mango']
+for index in range(len(fruits)):
+    print '当前水果 ：', fruits[index]
+
+print "Good bye!"
+
+"""
+$ python Basic/for-2.py
+当前水果 ： banana
+当前水果 ： apple
+当前水果 ： mango
+Good bye!
+
+"""
