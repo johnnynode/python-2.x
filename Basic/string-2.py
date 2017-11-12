be0e04d74484e064eb18fb8551b89ac9ce38db43
johnnynode <johnnynode@gmail.com>
2017-11-12 11:47:38 +0800

    string

---
+++ Basic/string-2.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+var1 = 'Hello World!'
+var2 = 'Python Runoob'
+
+print "var1[0]: ", var1[0]
+print "var2[1:5]: ", var2[1:5]
+
+"""
+MacBook-Pro:python-2.x Johnny$ python Basic/string-2.py
+var1[0]:  H
+var2[1:5]:  ytho
+"""
