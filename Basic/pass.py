917f0896b4811fa21b1ff451fd7029db226d0eeb
johnnynode <johnnynode@gmail.com>
2017-11-12 11:43:07 +0800

    pass

---
+++ Basic/pass.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+for letter in 'Python':
+  if letter == 'h':
+    pass
+    print '这是pass块'
+  print '当前字母 :', letter
+
+print "Good bye!"
+
+"""
+当前字母 : P
+当前字母 : y
+当前字母 : t
+这是pass块
+当前字母 : h
+当前字母 : o
+当前字母 : n
+Good bye!
+"""
