9b4e686c648998cf36b0bff4ad01d419eed4d9a4
johnnynode <johnnynode@gmail.com>
2017-11-16 09:52:23 +0800

    date-time

---
+++ Basic/date-time-3.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+
+import time
+
+localtime = time.asctime(time.localtime(time.time()))
+print "本地时间为 ：", localtime
+
+"""
+$ python Basic/date-time-3.py
+本地时间为 ： Thu Nov 16 09:51:45 2017
+"""
