534c4df667c44a9f6ed1c43a4b665a6707cf6d91
johnnynode <johnnynode@gmail.com>
2017-11-16 09:57:59 +0800

    date-time

---
+++ Basic/date-time-5.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+import calendar
+
+cal = calendar.month(2017,1)
+print "以下输入2016年1月份的日历："
+print cal;
+
+"""
+$ python Basic/date-time-5.py
+以下输入2016年1月份的日历：
+    January 2017
+Mo Tu We Th Fr Sa Su
+                   1
+ 2  3  4  5  6  7  8
+ 9 10 11 12 13 14 15
+16 17 18 19 20 21 22
+23 24 25 26 27 28 29
+30 31
+"""
