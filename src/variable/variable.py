9d4d76a8999d579d67e5987d5bf562c88bed66ba
johnnynode <johnnynode@gmail.com>
2018-01-02 06:40:52 +0800

    test variable1

---
+++ src/variable/variable.py
@@ -0,0 +1,14 @@
+#coding=utf-8
+
+# 测试 变量赋值
+def test1():
+    counter = 100  # 赋值整型变量
+    miles = 1000.0  # 浮点型
+    name = "John"  # 字符串
+
+    print counter
+    print miles
+    print name
+
+# 执行函数
+test1()
