f6be3893952df86298858165f00d7ddf3e22288a
johnnynode <johnnynode@gmail.com>
2018-01-02 06:47:18 +0800

    test variable2

--- src/variable/variable.py
+++ src/variable/variable.py
@@ -9,6 +9,20 @@ def test1():
     print counter
     print miles
     print name
+    print 
+
+# 测试 变量赋值
+def test2():
+    str = 'Hello World!'
+ 
+    print str           # 输出完整字符串
+    print str[0]        # 输出字符串中的第一个字符
+    print str[2:5]      # 输出字符串中第三个至第五个之间的字符串
+    print str[2:]       # 输出从第三个字符开始的字符串
+    print str * 2       # 输出字符串两次
+    print str + "TEST"  # 输出连接的字符串
+
 
 # 执行函数
 test1()
+test2()
